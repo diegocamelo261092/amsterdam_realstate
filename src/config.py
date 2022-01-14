2dc1092abbcdd62df2f8df2fe3ed55f823c89109
DanielFranciscoSalazar <daniel@hubs.com>
2022-01-14 21:21:12 +0100

    Comments and small code changes

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-mysql_user = 'admin'
-mysql_password = '261092Dmc!'
+mysql_user = 'daniel_admin'
+mysql_password = 'k13urw80fusa'
 mysql_db_name = 'amsterdam_realstate'
 mysql_db_host_name = 'amsterdam-realstate.cwti0quxqm5p.us-east-2.rds.amazonaws.com'
